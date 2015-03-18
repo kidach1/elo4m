d37c49d44a71fc3c8338695efd211433a76acc19
Daiki Taniguchi <daiki.taniguchi@aktsk.jp>
2015-03-18 20:28:26 +0900

    add rating.rb

---
+++ lib/elo4m/rating.rb
@@ -0,0 +1,35 @@
+module Elo4m
+  class Rating
+    include Helper
+
+    attr_reader :other_rating
+    attr_reader :old_rating
+    attr_reader :k_factor
+    attr_reader  :expected
+
+    def new_rating
+      (old_rating.to_f + change).to_i
+    end
+
+    # The expected score is the probably outcome of the match, depending
+    # on the difference in rating between the two players.
+    def expected_al
+      1.0 / ( 1.0 + ( 10.0 ** ((other_rating.to_f - old_rating.to_f) / 400.0) ) )
+    end
+
+    private
+
+    def result
+      @result.to_f
+    end
+
+    def valid_result?
+      (0..1).include? @result
+    end
+
+    def change
+      expected = self.expected.nil? ? expected_al : self.expected
+      k_factor.to_f * ( result.to_f - expected )
+    end
+  end
+end
