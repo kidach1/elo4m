7997fff51f7d3d977d52b22f334d2d1abda9421e
Daiki Taniguchi <daiki.taniguchi@aktsk.jp>
2015-03-18 20:32:42 +0900

    add entry point

--- lib/elo4m.rb
+++ lib/elo4m.rb
@@ -1,5 +1,16 @@
 require "elo4m/version"
+require "elo/helper"
+require "elo/configuration"
+require "elo/game"
+require "elo/player"
+require "elo/rating"
 
 module Elo4m
-  # Your code goes here...
+  def self.config
+    @config ||= Configuration.new
+  end
+
+  def self.configure(&block)
+    yield(config)
+  end
 end
